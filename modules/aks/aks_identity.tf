daa55373698a76492432b0523bcc2c8b707aa8f7
Chinmay Jog <chinmaymjog@gmail.com>
2025-01-23 07:32:18 +0530

    Commented role mapping

--- modules/aks/aks_identity.tf
+++ modules/aks/aks_identity.tf
@@ -18,22 +18,22 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = var.hub_rgname
 }
 
-resource "azurerm_role_assignment" "rg-role" {
-  principal_id                     = azurerm_user_assigned_identity.identity.principal_id
-  role_definition_name             = "Contributor"
-  scope                            = azurerm_resource_group.rg_aks.id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "rg-role" {
+#   principal_id                     = azurerm_user_assigned_identity.identity.principal_id
+#   role_definition_name             = "Contributor"
+#   scope                            = azurerm_resource_group.rg_aks.id
+#   skip_service_principal_aad_check = true
+# }
 
-resource "azurerm_role_assignment" "acr-role" {
-  principal_id                     = data.azurerm_user_assigned_identity.aks-agentpool.principal_id
-  role_definition_name             = "AcrPull"
-  scope                            = data.azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes = [
-      principal_id,
-    ]
-  }
-}
+# resource "azurerm_role_assignment" "acr-role" {
+#   principal_id                     = data.azurerm_user_assigned_identity.aks-agentpool.principal_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = data.azurerm_container_registry.acr.id
+#   skip_service_principal_aad_check = true
+#   lifecycle {
+#     prevent_destroy = false
+#     ignore_changes = [
+#       principal_id,
+#     ]
+#   }
+# }
